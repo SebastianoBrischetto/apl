942802172459b927c0b06bae40e4244653188923
Sebastiano Brischetto <sebybrischetto@gmail.com>
2024-01-03 12:53:57 +0100

    refactor(refactor parity): refactor parity

--- cpp/src/include/classes/parity/parity.cpp
+++ cpp/src/include/classes/parity/parity.cpp
@@ -1,16 +1,20 @@
 #include "parity.h"
 
-Parity::Parity(Ocean& ocean, Fleet& fleet) : HuntAndTarget(ocean, fleet){}
+Parity::Parity(Ocean& ocean, Fleet& fleet) : 
+    HuntAndTarget(ocean, fleet) 
+{}
 
-void Parity::hunt(){
-    while(true){
+void Parity::hunt() {
+    while (true) {
         int x = rand() % columns_;
         int y = rand() % rows_;
 
-        if((x & 1) != (y & 1)){     //se x e y hanno parita diversa (lsb diverso) incrementa di 1 y in modo da modificarne la parita
+        // Se x e y hanno parità diversa (LSB diverso), incrementa y di 1 per modificarne la parità
+        if ((x & 1) != (y & 1)) {
             y = (++y) % rows_;
         }
-        if(hitAndNotify(x,y)){
+
+        if (hitAndNotify(x, y)) {
             addCloseCells(x, y);
             break;
         }
--- cpp/src/include/classes/parity/parity.h
+++ cpp/src/include/classes/parity/parity.h
@@ -5,22 +5,22 @@
 
 /**
  * @class Parity
- * @brief Algoritmo che esegue una prima fase di caccia colpendo le celle con la stessa parita, e dopo aver trovato un bersaglio colpisce intorno ad esso.
+ * @brief Algoritmo che esegue una prima fase di caccia colpendo le celle con la stessa parità, e dopo aver trovato un bersaglio colpisce intorno ad esso.
  */
-class Parity: public HuntAndTarget{
+class Parity : public HuntAndTarget {
 
 public:
     /**
-     * @brief Costruttore per il bot parity.
+     * @brief Costruttore per il bot Parity.
      * 
      * @param ocean Riferimento all'oceano nemico.
      * @param fleet Riferimento alla flotta nemica.
      */
     Parity(Ocean& ocean, Fleet& fleet);
-    
+
 protected:
     /**
-     * @brief Esegue la fase di caccia della strategia (colpisce le celle che hanno le cordinate con la stessa parita).
+     * @brief Esegue la fase di caccia della strategia (colpisce le celle che hanno le coordinate con la stessa parità).
      */
     void hunt() override;
 };
