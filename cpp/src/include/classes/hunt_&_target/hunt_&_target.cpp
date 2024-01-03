23374f12c0c4e1479cd0daf42012aedf079776e1
Sebastiano Brischetto <sebybrischetto@gmail.com>
2024-01-03 12:50:20 +0100

    refactor(refactor hunt and target): refactor hunt and target

--- cpp/src/include/classes/hunt_&_target/hunt_&_target.cpp
+++ cpp/src/include/classes/hunt_&_target/hunt_&_target.cpp
@@ -1,52 +1,60 @@
 #include "hunt_&_target.h"
 
-HuntAndTarget::HuntAndTarget(Ocean& ocean, Fleet& fleet) : BotAlgorithm(ocean, fleet){}
+HuntAndTarget::HuntAndTarget(Ocean& ocean, Fleet& fleet) : 
+    BotAlgorithm(ocean, fleet)
+{}
 
-void HuntAndTarget::doMove(){
-    if(counter_ >= columns_ * rows_){
+void HuntAndTarget::doMove() {
+    if (counter_ >= columns_ * rows_) {
         return;
     }
-    if(target_list_.empty()){
+    if (target_list_.empty()) {
         hunt();
-    }else{
+    } else {
         target();
     }
     counter_++;
 }
 
-void HuntAndTarget::hunt(){
-    while(true){
-        int x = rand()%columns_;
-        int y = rand()%rows_;
-        if(hitAndNotify(x, y)){
+void HuntAndTarget::hunt() {
+    while (true) {
+        int x = rand() % columns_;
+        int y = rand() % rows_;
+        if (hitAndNotify(x, y)) {
             addCloseCells(x, y);
             break;
         }
     }
 }
 
-void HuntAndTarget::addCloseCells(int x, int y){
-    if(ocean_.getCell(x,y).getIsOccupied()){
-        addToTargets(x, y - 1);    //UP
-        addToTargets(x + 1, y);    //RIGHT
-        addToTargets(x, y + 1);    //DOWN
-        addToTargets(x - 1, y);    //LEFT
+void HuntAndTarget::addCloseCells(int x, int y) {
+    if (ocean_.getCell(x, y).getIsOccupied()) {
+        for(Direction dir : {UP, RIGHT, DOWN, LEFT}){
+            int new_target_x;
+            int new_target_y;
+            Ocean::updateCoordinates(x, y, new_target_x, new_target_y, 1, dir);
+            addToTargets(new_target_x, new_target_y);
+        }
     }
 }
 
-void HuntAndTarget::addToTargets(int x, int y){
-    if((x >= 0 && x < columns_) && (y >= 0 && y < rows_) && !ocean_.getCell(x, y).getIsHit()){
+void HuntAndTarget::addToTargets(int x, int y) {
+    if (ocean_.areCoordinatesValid(x, y) && !ocean_.getCell(x, y).getIsHit()) {
         target_list_.emplace_back(ocean_.getCell(x, y));
     }
 }
 
-void HuntAndTarget::target(){
-    if(!target_list_.front().get().getIsHit()){
-        int x = target_list_.front().get().getXCoord();
-        int y = target_list_.front().get().getYCoord();
-        if(hitAndNotify(x,y)){
-            addCloseCells(x, y);
+void HuntAndTarget::target() {
+    while(true){
+        Cell& last_target = target_list_.front().get();
+        target_list_.pop_front();
+        if (!last_target.getIsHit()) {
+            int x = last_target.getXCoord();
+            int y = last_target.getYCoord();
+            if (hitAndNotify(x, y)) {
+                addCloseCells(x, y);
+            }
+            break;
         }
     }
-    target_list_.pop_front();
 }
--- cpp/src/include/classes/hunt_&_target/hunt_&_target.h
+++ cpp/src/include/classes/hunt_&_target/hunt_&_target.h
@@ -9,11 +9,11 @@
  * @class HuntAndTarget
  * @brief Algoritmo che esegue una prima fase di caccia colpendo in modo casuale, e dopo aver trovato un bersaglio colpisce intorno ad esso.
  */
-class HuntAndTarget : public BotAlgorithm{
+class HuntAndTarget : public BotAlgorithm {
 
 public:
     /**
-     * @brief Costruttore per il bot hunt&target.
+     * @brief Costruttore per il bot Hunt&Target.
      * 
      * @param ocean Riferimento all'oceano nemico.
      * @param fleet Riferimento alla flotta nemica.
@@ -21,7 +21,7 @@ public:
     HuntAndTarget(Ocean& ocean, Fleet& fleet);
 
     /**
-     * @brief esegue la strategia del bot (caccia e bersaglia).
+     * @brief Esegue la strategia del bot (caccia e bersaglia).
      */
     void doMove() override;
     
@@ -34,16 +34,16 @@ protected:
     /**
      * @brief Aggiunge le celle adiacenti alla cella contenente una nave trovata.
      * 
-     * @param x cordinata x della cella trovata.
-     * @param y cordinata y della cella trovata.
+     * @param x Coordinata x della cella trovata.
+     * @param y Coordinata y della cella trovata.
      */
     void addCloseCells(int x, int y);
 
     /**
-     * @brief Aggiunge la cella con le coordinate x,y alla lista dei bersagli.
+     * @brief Aggiunge la cella con le coordinate x, y alla lista dei bersagli.
      * 
-     * @param x cordinata x della cella.
-     * @param y cordinata y della cella.
+     * @param x Coordinata x della cella.
+     * @param y Coordinata y della cella.
      */
     virtual void addToTargets(int x, int y);
 
@@ -52,7 +52,7 @@ protected:
      */
     virtual void target();
 
-    std::deque<std::reference_wrapper<Cell>> target_list_;  ///< lista dei bersagli.
+    std::deque<std::reference_wrapper<Cell>> target_list_;  ///< Lista dei bersagli.
 };
 
 #endif
