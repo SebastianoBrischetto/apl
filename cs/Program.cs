021c5bd978347d26ab11925a15214c8ecb586fe7
DanieleZinghirino <daniele_zinghirino@hotmail.it>
2024-01-15 18:30:35 +0100

    Client cs

---
+++ cs/Program.cs
@@ -0,0 +1,24 @@
+﻿//QUELLO DA TESTARE
+using cs.Logic.GameLogic.Managers;
+using cs.Logic.UserLogic;
+namespace cs
+{
+    static class Program
+    {
+        private static async Task Main()
+        {
+            await GameManager.Login();
+            Console.WriteLine(
+                "Vuoi creare una nuova partita (start) o partecipare ad una partita già avviata (join)?: ");
+            string mode = Console.ReadLine();
+            if (mode == "start")
+            {
+                await GameManager.InizializeGame(User.Instance.Username, User.Instance.Token);
+            }
+            else
+            {
+                await GameManager.JoinGame();
+            }
+        }
+    }
+}
